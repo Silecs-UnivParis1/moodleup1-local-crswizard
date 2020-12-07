4a5793134bdcf415a5a70b6d990d76d2eecd7635
Guillaume Allegre <guillaume.allegre@silecs.info>
2020-12-07 14:48:53 +0100

    (0ac1e473de) [SMT]  Date de fin de cours à la fin du semestre

--- step2_form.php
+++ step2_form.php
@@ -90,7 +90,8 @@ class course_wizard_step2_form extends moodleform {
 
         $datefermeture = 'up1datefermeture';
         $mform->addElement('date_selector', $datefermeture, get_string('up1datefermeture', 'local_crswizard'));
-        $mform->setDefault($datefermeture, time());
+        $fin_semestre = strtotime(date('m') <= 6 ? "July 31" : "next year January 31");
+        $mform->setDefault($datefermeture, $fin_semestre);
 
         $mform->addElement('header', 'URL', 'Souhaitez-vous utiliser une URL pérenne ?');
         $mform->setExpanded('URL');
