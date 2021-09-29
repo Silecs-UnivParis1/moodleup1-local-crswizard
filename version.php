5b9efc6a67a3d9f3081679a203cb6c37dba9d053
Guillaume Allegre <guillaume.allegre@silecs.info>
2021-09-29 17:11:30 +0200

    Suppression du menu navigation (pris en charge par block_up1_creation)

--- version.php
+++ version.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package    local_crswizard
- * @copyright  2012-2020 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2021 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2021060600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2021092900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020060900;        // Requires this Moodle version
 $plugin->component = 'local_crswizard';       // Full name of the plugin (used for diagnostics)
 
