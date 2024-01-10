5d54a2808bdc609f3e1dcc177c56e80c8d7a0180
Guilloux <cecile.guilloux@silecs.info>
2024-01-10 17:34:20 +0100

    M#5752 cours - lien Paramètres caché

--- lib.php
+++ lib.php
@@ -10,3 +10,20 @@ require_once(__DIR__ . '/libaccess.php');
 
 // Depuis Moodle 3.9, on n'utilise plus le menu Navigation de Moodle
 // À la place, on utilise le bloc "Création de cours"
+
+function local_crswizard_extend_navigation_course($navigation, $course, $context) {
+    global $PAGE, $USER;
+    if (get_capability_info('local/up1_capabilities:course_updatesettings')) {
+        if (!has_capability('local/up1_capabilities:course_updatesettings', $context, $USER, true)) {
+            if ($PAGE->theme->name == 'adaptable') {
+                $children = $navigation->get_children_key_list();
+                if (count($children) && in_array('editsettings', $children)) {
+                    $editsettings = $navigation->find('editsettings', navigation_node::TYPE_SETTING);
+                    if ($editsettings) {
+                        $editsettings->hide();
+                    }
+                }
+            }    
+        }
+    }
+}
