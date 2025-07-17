66f5999364f96a271eebbd2c53c82eef6b23af1a
Isham Yagoub <isham.yagoub@univ-paris1.fr>
2025-07-17 16:36:59 +0200

    Ne plus rendre dépendant du thème l'accès au menu de paramètre

--- lib.php
+++ lib.php
@@ -15,15 +15,14 @@ function local_crswizard_extend_navigation_course($navigation, $course, $context
     global $PAGE, $USER;
     if (get_capability_info('local/up1_capabilities:course_updatesettings')) {
         if (!has_capability('local/up1_capabilities:course_updatesettings', $context, $USER, true)) {
-            if ($PAGE->theme->name == 'adaptable') {
-                $children = $navigation->get_children_key_list();
-                if (count($children) && in_array('editsettings', $children)) {
-                    $editsettings = $navigation->find('editsettings', navigation_node::TYPE_SETTING);
-                    if ($editsettings) {
-                        $editsettings->hide();
-                    }
+            $children = $navigation->get_children_key_list();
+            if (count($children) && in_array('editsettings', $children)) {
+                $editsettings = $navigation->find('editsettings', navigation_node::TYPE_SETTING);
+                if ($editsettings) {
+                    $editsettings->hide();
                 }
-            }    
+            }
+              
         }
     }
 }
