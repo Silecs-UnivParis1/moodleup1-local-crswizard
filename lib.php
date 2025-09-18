cf74d126d4655ff9aef6bbcb7a72d16731a4148c
Guilloux <cecile.guilloux@silecs.info>
2025-09-18 22:01:51 +0200

    M#5752 code JS adapté au thème boost_union

--- lib.php
+++ lib.php
@@ -22,15 +22,20 @@ function local_crswizard_extend_navigation_course($navigation, $course, $context
                     $editsettings->hide();
                 }
             }
-             $PAGE->requires->js_init_code(<<<EOJS
-                let selecteur = '#region-main div.secondary-navigation ul[role="menubar"]';
-                let navcouse = document.querySelectorAll(selecteur + ' li[data-key="coursehome"]');
-                let navparam = document.querySelectorAll(selecteur + ' li[data-key="editsettings"]');
+            $selecteur = '#page div.main-inner div.secondary-navigation ul[role="menubar"]';
+            if ($PAGE->theme->name == 'adaptable') {
+                $selecteur = '#region-main div.secondary-navigation ul[role="menubar"]';
+            }
+            $selenc = json_encode($selecteur);
+            $PAGE->requires->js_init_code(<<<EOJS
+                console.log($selenc);
+                let navcouse = document.querySelectorAll($selenc + ' li[data-key="coursehome"]');
+                let navparam = document.querySelectorAll($selenc + ' li[data-key="editsettings"]');
                 if (navcouse.length == 1 && navparam.length == 1) {
                     navparam[0].remove();
                 }
 EOJS
-        , true);
+        , false);
         }
     }
 }
