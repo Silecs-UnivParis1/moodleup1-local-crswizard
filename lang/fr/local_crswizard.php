1dab3ae543f57f6472261dff0e513c465f8d7f84
Guilloux <cecile.guilloux@silecs.info>
2026-06-03 12:06:07 +0200

    ++

--- lang/fr/local_crswizard.php
+++ lang/fr/local_crswizard.php
@@ -16,7 +16,7 @@ $string['crswizard:localsupervalidator'] = 'Approuver n\'importe quel cours cré
 $string['crswizard:rofreferenceeditor'] = 'Modifier les rattachements au ROF d\'un cours';
 $string['crswizard:hybridattachment'] = 'Ajouter/modifier des rattachements au ROF dans cours hybrides';
 $string['crswizard:duplicate'] = 'Dupliquer le cours avec l\'assistant.';
-$string['crswizard:updaterofsyllabus'] = 'Modifier les dans le syllabus d\'un cours la valeur des champs provenant du ROF';
+$string['crswizard:updaterofsyllabus'] = 'Modifier dans le syllabus d\'un cours la valeur des champs provenant du ROF';
 
 $string['addsyllabusstep'] = 'Ajout de l\'étape Syllabus';
 $string['assessmentsettings'] = 'Modalités d\'évaluation';
